25e23bd42134d29736b2f3e7242eb50eb01f36ae
CraigWH <craigwesleyharvey@gmail.com>
2024-05-14 12:14:14 +1000

    Main Menu Buttons (Basic)

--- App/app/src/main/java/com/example/reciperealm/MainActivity.java
+++ App/app/src/main/java/com/example/reciperealm/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.reciperealm;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +14,55 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button recipeListbtn, favRecipebtn, plannerbtn, profilebtn;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_profile);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        setContentView(R.layout.activity_main);
+
+        recipeListbtn = findViewById(R.id.RecipeList);
+        favRecipebtn = findViewById(R.id.FavRecipes);
+        plannerbtn = findViewById(R.id.Planner);
+        profilebtn = findViewById(R.id.Profile);
+
+        recipeListbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // opening a new activity on button click
+                Intent i = new Intent(MainActivity.this, RecipeList.class);
+                startActivity(i);
+            }
         });
+
+        favRecipebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // opening a new activity on button click
+                Intent i = new Intent(MainActivity.this,FavoriteRecipes.class);
+                startActivity(i);
+            }
+        });
+
+        plannerbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // opening a new activity on button click
+                Intent i = new Intent(MainActivity.this,Planner.class);
+                startActivity(i);
+            }
+        });
+
+        profilebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // opening a new activity on button click
+                Intent i = new Intent(MainActivity.this,Profile.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
